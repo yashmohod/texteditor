ecf93fb065572b97fe842363d090bf9a1f12fc75
Yash Mohod <mohodyash@gmail.com>
2026-01-05 10:21:47 -0500

    up

---
+++ kilo.c
@@ -0,0 +1,94 @@
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+
+/*** data ***/
+struct termios orig_termios;
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+
+/*** terminal ***/
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+
+void disableRawMode(){ 
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode(){
+ 
+    if (tcgetattr(STDIN_FILENO, &orig_termios)== -1) die("tcgetattr");
+    atexit(disableRawMode);
+    
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN );
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 10;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)== -1) die("tcsetattr");
+ 
+
+}
+
+char editorReadKey(){
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
+       if(nread == -1 && errno != EAGAIN) die("read"); 
+    }
+    return c;
+}
+
+
+
+/*** input ***/
+void editorProcessKeypress(){
+    char c = editorReadKey();
+
+    switch(c){
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+
+/*** output ***/
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+}
+
+/*** init ***/
+int main(){
+    enableRawMode();
+
+    while(1) {
+        editorRefreshScreen();
+        editorProcessKeypress();
+    }
+    
+
+
+
+    return 0;
+}
+
+
+
+
